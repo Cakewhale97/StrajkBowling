6162b2b84bd5b55139c895a758054f352ee18108
JassinAlSafe <121439769+Cakewhale97@users.noreply.github.com>
2024-05-28 12:10:42 +0200

    update

--- StarBowling/src/setupTest.js
+++ StarBowling/src/setupTest.js
@@ -2,8 +2,19 @@
 import { Response, Request, Headers } from 'node-fetch';
 import fetch from "node-fetch";
 import '@testing-library/jest-dom';
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { expect } from 'expect';
+console.log('expect', expect); 
 
 global.Response = Response;
 global.Request = Request;
 global.Headers = Headers;
 global.fetch = fetch;
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
